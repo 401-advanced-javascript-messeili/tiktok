7c11167ef261d70b7b47d5cc32954b7d025ddca5
messeili <mymoa.2014@gmail.com>
2020-12-06 02:17:21 +0200

    setup amplify and api

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import Home from './src/screens/home';
 import 'react-native-gesture-handler';
 import Navigation from './src/navigation';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 export default function App() {
   return (
